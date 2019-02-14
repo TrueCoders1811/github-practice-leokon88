4968f564e15759d326693834145e8cce798a0797
YShin <yookyung.shin@gmail.com>
2019-02-13 18:11:46 -0600

    basic console created

--- DisplayInfo/Program.cs
+++ DisplayInfo/Program.cs
@@ -6,12 +6,24 @@ namespace DisplayInfo
     {
         static void Main(string[] args)
         {
-            // Get first and last name from user input
-            // Get their favorite number
-            // Get their favorite animal
+            Console.WriteLine("Tell me about your self");// Get first name from user input
+
+            Console.WriteLine("What is your first name?");// Get first name from user input
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");// Get last name from user input
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite number?");//Get their favorite number
+            int favNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your favorite animal?");// Get their favorite animal
+            string favAnimal = Console.ReadLine();
 
             // Once you have gotten all their info and stored it,
             // display it back to them.
+            
+
+            Console.WriteLine($"{firstName} {lastName}'s favorite number is {favNumber} and the favorite animal is {favAnimal}" );
 
             // If their favorite number is greater than 10, display it in green.
             // If their favorite number is less than 5, display it in red.
