3fe14e0adbbbba1d67019dd4ee580f1ef1dc8023
YShin <yookyung.shin@gmail.com>
2019-02-14 19:43:22 -0600

    added lines to change the color of the favorite number. If input favorite number is greater than 10, the console will display green colored number. If input favorite number is less than 5, the console will display red colored number. Number between 5 and 10 is blue text color.

--- DisplayInfo/Program.cs
+++ DisplayInfo/Program.cs
@@ -6,7 +6,7 @@ namespace DisplayInfo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tell me about your self");// Get first name from user input
+            Console.WriteLine("Tell me about your self");
 
             Console.WriteLine("What is your first name?");// Get first name from user input
             string firstName = Console.ReadLine();
@@ -15,26 +15,36 @@ namespace DisplayInfo
 
             Console.WriteLine("What is your favorite number?");//Get their favorite number
             int favNumber = int.Parse(Console.ReadLine());
-            if (favNumber > 10)
-            {
-                favNumber.Console.ForegroundColor = ConsoleColor.Green;
-                Console.Clear();
-            }
-            //else if (favNumber < 5)
-            //{
-            //    Console.ForegroundColor = ConsoleColor.Red;
-            //}
+            
+
 
             Console.WriteLine("What is your favorite animal?");// Get their favorite animal
             string favAnimal = Console.ReadLine();
 
             // Once you have gotten all their info and stored it,
             // display it back to them.
-            
-             Console.WriteLine($"{firstName} {lastName}'s favorite number is {favNumber} and the favorite animal is {favAnimal}" );
 
-            // If their favorite number is greater than 10, display it in green.
-            // If their favorite number is less than 5, display it in red.
+            Console.Write($"{firstName} {lastName}'s favorite number is ");
+            if (favNumber > 10)     // If their favorite number is greater than 10, display it in green.
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                
+            }
+            else if (favNumber < 5)// If their favorite number is less than 5, display it in red.
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else // If their favorite number is between 5 and 10, display it in blue.
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            Console.Write(favNumber);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write($" and the favorite animal is {favAnimal}" );
+            Console.WriteLine();
+
+        
+            
 
         }
     }
